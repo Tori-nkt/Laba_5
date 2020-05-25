2071b2cc5df3b9f330939bb4d24b8daee0810b4e
elsbrd <56909624+elsbrd@users.noreply.github.com>
2020-05-26 00:37:15 +0300

    First commit
    First commit. Reading from the current file

---
+++ Laba_5/Laba_5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Laba_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+        }
+
+        public static string NewInput()
+        {
+            Console.WriteLine("Входные данные:");
+            int count = -1;
+            StreamReader file = new StreamReader("File.txt");
+            string line;
+            //ArrayList arrstr = new ArrayList();
+            List<string> strArr = new List<string>();
+            while ((line = file.ReadLine()) != null)
+            {
+                strArr.Add(line);
+                Console.WriteLine(line);
+                count++;
+            }
+            string result = strArr[count];
+            strArr.RemoveAt(count);
+            for (int i = 0; i < strArr.Count; i++)
+            {
+                string[] str = strArr[i].Split(" = ");
+                result = result.Replace(str[0], str[1]);
+            }
+
+            return result;
+        }
+    }
+}
