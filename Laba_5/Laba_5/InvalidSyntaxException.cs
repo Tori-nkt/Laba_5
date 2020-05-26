dddd832760db705708d1f0bed422073fd4c5bb17
Tori-nkt <Tori-nkt@users.noreply.github.com>
2020-05-27 02:10:45 +0300

    Create InvalidSyntaxException.cs

---
+++ Laba_5/Laba_5/InvalidSyntaxException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Лабораторная_5
+{
+
+    [Serializable]
+    internal class InvalidSyntaxException : Exception
+    {
+        public InvalidSyntaxException()
+        {
+        }
+
+        public InvalidSyntaxException(string message) : base(message)
+        {
+        }
+
+        public InvalidSyntaxException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidSyntaxException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
